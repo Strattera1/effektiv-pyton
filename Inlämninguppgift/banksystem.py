c4ee0351331c9beb732e2d9904772e43362c402e
Nils Paulson <nille.paulson@gmail.com>
2023-09-20 15:20:55 +0200

    version-2 is done

--- Inlämninguppgift/banksystem.py
+++ Inlämninguppgift/banksystem.py
@@ -4,7 +4,6 @@ from time import time, sleep
 import datetime
 from dataclasses import dataclass
 
-#Skapa först en list comprainsion sen en dict. Använd Classen som en key. Då blir de andra values. 
 @dataclass
 class Customer:
 
@@ -47,7 +46,6 @@ def find_account(customer_dict:dict[Customer],
     result = customer_dict.get(account_number)
     return result
 
-
     
 def get_customer_birthday() -> datetime.date:
     year = random.randint(1922,2000)
@@ -63,41 +61,42 @@ def format_account_number(count:int)-> str:
 
 if __name__ == "__main__":
 
-    
+    milseconds = 1000
     start = time()
     customer_dict= {
-        format_account_number(i): Customer(account_number=format_account_number(i), name=i,birthday=get_customer_birthday(), saldo=0) for  i in range(10**7)
+        format_account_number(i): Customer(account_number=format_account_number(i),
+                                            name=i,birthday=get_customer_birthday(), saldo=0) for  i in range(10**7)
         }  
-    #customers = []
+      
     
-    #[customer_dict[x.account_number]=  x.customer]
-    #print(customer_dict)
-    # for i in range(10**7):
-    #     customer_birthday = get_customer_birthday()
-    #     customer = Customer(account_number=i, name=i, birthday=customer_birthday, saldo=int)
-    #     print(customer)
-        #customer_dict[customer.account_number] = customer
-        #customer_dict.keys[customer.account_number]
-    #Verkar som att jag har ett problem här. Eftersom i skapar kunderna i version 1, blir det något konstigt här. 
-
-
-
     end = time()
     elapsed_time_for_creating_customers = end - start
+    print(f"this is how long it took to create all customers: {elapsed_time_for_creating_customers:.5f}{elapsed_time_for_creating_customers * 1000:2f} ms elsapsed")
 
-    print(f"this is how long it took to create all customers: {elapsed_time_for_creating_customers} seconds elsapsed")
+    total_time_1 = 0
+    for m in range(milseconds):
+        start = time()
+        result_account1 = find_account(customer_dict,account_number="1111-0000001000")
+        end = time()
+        total_time_1 += (end-start)
+    result_time_1 = total_time_1/milseconds
+    print ("result: ",result_account1, f" {result_time_1:.5f} {result_time_1 * 1000:2f} millesceonds")
 
-    start = time()
-    result_account1 = find_account(customer_dict,account_number="1111-0000001000")
-    end = time()
-    print ("result: ",result_account1, f" seconds to find {end-start}")
 
-    start = time()
-    result_account2 = find_account(customer_dict,account_number="1111-0009999999")
-    end= time()
-    print ("result: ",result_account2, f" seconds to find {end-start}")
+    total_time_2 = 0
+    for m in range(milseconds):
+        start = time()
+        result_account2 = find_account(customer_dict,account_number="1111-0009999999")
+        end = time()
+        total_time_2 += (end-start)
+    result_time_2 = total_time_1/milseconds
+    print("Result:", result_account2, f" {result_time_2:.5f}{result_time_2 * 1000:2f} millesceonds to find")
     
-    start = time()
-    result_account3 = find_account(customer_dict,account_number="1111-9999999999")
-    end = time()
-    print ("result: ",result_account3, f" seconds to find {end-start}, account does not exist.")
+    total_time_3 = 0
+    for x in range(milseconds):
+        start = time()
+        result_account3 = find_account(customer_dict,account_number="1111-9999999999")
+        end = time()
+        total_time_3 += (end-start)
+    result_time_3 = total_time_3/milseconds
+    print("Result:", result_account3, f"{result_time_3:.5f}{result_time_3 *1000:2f} mileseconds,  account does not exist.")
